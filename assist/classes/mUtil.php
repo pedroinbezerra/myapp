efeb814dd14019af38234bb0980151998316b536
pedroinbezerra <pedroigornobre@outlook.com>
2021-04-06 21:40:45 -0300

    Criação da função de converter real para centavos

--- assist/classes/mUtil.php
+++ assist/classes/mUtil.php
@@ -6,5 +6,17 @@ class mUtil {
     public function autoCaption($string){
         return ucfirst(strtolower($string));
     }
+
+    public function convertRealToCents($value){
+        $result = '';
+
+        try {
+            $result = $value / 100;
+        } catch (Exception $e) {
+            $result = 'Erro ao converter o valor para centavos';
+        }
+
+        return $result;
+    }
     
 }
